129a9928ed2280bb6eb638c892a729024e6389d4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-03-07 00:26:56 +0900

    Update Equalize the Array.cpp

--- Equalize the Array.cpp
+++ Equalize the Array.cpp
@@ -53,3 +53,36 @@ int equalizeArray1(vector<int> arr)
     
     return arr.size() - max;
 }
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = equalizeArray(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+
